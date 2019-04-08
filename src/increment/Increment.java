d752327001a0805e4949630f4d9917251532a402
tsk-nkgw <tasktas9@gmail.com>
2019-04-08 13:56:02 +0900

    add new statement

--- src/increment/Increment.java
+++ src/increment/Increment.java
@@ -4,6 +4,7 @@ public class Increment extends Thread {
     static volatile int x = 0;
     public void run(){
         int tmp = x;
+        Thread.yield();
         x = tmp + 1;
 
         System.out.println(x);
