564091ea361635693b015babfd393918c45255ac
tsk-nkgw <tasktas9@gmail.com>
2019-04-08 13:59:13 +0900

    add comment

--- src/increment/Main.java
+++ src/increment/Main.java
@@ -7,3 +7,14 @@ public class Main {
         System.out.println("Main " + Increment.x);
     }
 }
+
+/*
+    Thraed1    |    Thread2
+-------------------------------
+  int tmp = x  |
+               |  int tmp = x
+               |  x = tmp + 1
+  x = tmp + 1  |
+
+  bug!!!!!!
+ */
