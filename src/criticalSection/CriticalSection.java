7b3d9f31cf3b0beae5079e3d188b8a31406b1dac
tsk-nkgw <tasktas9@gmail.com>
2019-05-13 14:45:21 +0900

    answer

--- src/criticalSection/CriticalSection.java
+++ src/criticalSection/CriticalSection.java
@@ -5,14 +5,13 @@ import java.util.concurrent.Semaphore;
 public class CriticalSection extends Thread {
     private final Semaphore semaphore;
     private static final int NUM_OF_THREADS = 10;
-    private String name;
 
     private CriticalSection(final Semaphore semaphore) {
         this.semaphore = semaphore;
     }
 
     public void run() {
-        name = Thread.currentThread().getName();
+        final String name = Thread.currentThread().getName();
         while (true) {
             try {
                 Thread.sleep(500);
@@ -29,7 +28,7 @@ public class CriticalSection extends Thread {
     }
 
     public static void main(final String[] args) {
-        final Semaphore semaphore = new Semaphore(1);
+        final Semaphore semaphore = new Semaphore(2);
         for (int i = 0; i < NUM_OF_THREADS; i++) {
             new CriticalSection(semaphore).start();
         }
